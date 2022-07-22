1d3f30a4232c088382f918441469f4fb6b0b26db
karstenwinter <brox.p@web.de>
2022-07-22 13:55:57 +0200

    initial commit

---
+++ src/runFaceRegognition.ts
@@ -0,0 +1,60 @@
+import * as faceapi from 'face-api.js'
+import {canvas} from "./env"
+
+export type Box = { x: number, y: number, width: number, height: number }
+
+export let initialized = false
+export let recognitions = 0
+export let debug = false
+
+const faceDetectionNet = faceapi.nets.ssdMobilenetv1
+const minConfidence = 0.5
+
+const inputSize = 408
+const scoreThreshold = 0.5
+
+function getFaceDetectorOptions(net: faceapi.NeuralNetwork<any>) {
+    return net === faceapi.nets.ssdMobilenetv1
+        ? new faceapi.SsdMobilenetv1Options({minConfidence})
+        : new faceapi.TinyFaceDetectorOptions({inputSize, scoreThreshold})
+}
+
+export const faceDetectionOptions = getFaceDetectorOptions(faceDetectionNet)
+
+export async function init() {
+    if (initialized)
+        return
+    await faceDetectionNet.loadFromDisk('./weights')
+    initialized = true
+    console.log("initialized net just now")
+}
+
+export async function findFaces(url: string): Promise<Box[]> {
+    await init()
+
+    const img = await canvas.loadImage(url)
+    // await canvas.loadImage('./images/bbt1.jpg')
+    debug && console.log("img", img)
+    const canvas2 = new canvas.Canvas(img.width, img.height)
+    const ctx = canvas2.getContext('2d')
+    ctx.drawImage(img, 0, 0, img.width, img.height)
+
+    const detections = await faceapi.detectAllFaces(canvas2, faceDetectionOptions)
+    let results: Box[] = detections.map(x => {
+        let box = x.box
+        return ({
+            x: box.x,
+            y: box.y,
+            width: box.width,
+            height: box.height
+        })
+    })
+    debug && console.log("results", results)
+    recognitions++
+    return results
+}
+
+//async function testRun() {
+//    const url = "https://c1.scryfall.com/file/scryfall-cards/large/front/2/7/27855a38-a682-4f97-ad22-ac625e86faec.jpg?1631046745"
+//    console.log("url", url, await findFaces(url))
+//}
